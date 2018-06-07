d642da31be69dc4ce48b552e967ea79ca60c506c
name <mail@mail.mail>
2018-06-07 08:49:31 +0200

    Added code for reading from unix socket (commented out as it still doesn't compile)

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -14,7 +14,7 @@ ConnectionWrapper::ConnectionWrapper(QObject *parent, CPConnection *conn) : QObj
     QObject::connect(conn, &CPConnection::loginDetailsRequested, this, &ConnectionWrapper::onloginDetailsRequested);
     QObject::connect(conn, &CPConnection::waitingForStageEntry, this, &ConnectionWrapper::onwaitingForStageEntry);
     QObject::connect(conn, &CPConnection::stageEntrySuccessful, this, &ConnectionWrapper::stageEntrySuccessful);
-    QObject::connect(conn, &CPConnection::disconnected, this, &ConnectionWrapper::ondisconnected); 
+    QObject::connect(conn, &CPConnection::disconnected, this, &ConnectionWrapper::ondisconnected);
 }
 
 void ConnectionWrapper::startConnection()
@@ -107,6 +107,48 @@ void Controller::startCLI()
 
     QThread::sleep(5);
     fprintf(stderr, "Finished sleeping.\n");
+
+/*
+    int sock, msgsock, rval;
+    struct sockaddr_un server;
+    char buf[1024];
+
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        perror("opening stream socket");
+        exit(1);
+    }
+    server.sun_family = AF_UNIX;
+    strcpy(server.sun_path, NAME);
+    if (bind(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_un)))
+    {
+        perror("binding stream socket");
+        exit(1);
+    }
+    printf("Socket has name %s\n", server.sun_path);
+    listen(sock, 5);
+    for (;;)
+    {
+        msgsock = accept(sock, 0, 0);
+        if (msgsock == -1)
+            perror("accept");
+        else
+            do
+            {
+                bzero(buf, sizeof(buf));
+                if ((rval = read(msgsock, buf, 1024)) < 0)
+                    perror("reading stream message");
+                else if (rval == 0)
+                    printf("Ending connection\n");
+                else
+                    printf("-->%s\n", buf);
+            } while (rval > 0);
+        close(msgsock);
+    }
+    close(sock);
+    unlink(NAME);
+*/
 
     stdinNotifier = new QSocketNotifier(fileno(stdin), QSocketNotifier::Read, this);
 
