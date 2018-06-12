27b375fe1d1c4715ad1c98d97b5e99ce42d0c71f
name <mail@mail.mail>
2018-06-12 22:04:52 +0200

    message queue

--- src/web/gikocloud.js
+++ src/web/gikocloud.js
@@ -5,17 +5,12 @@ var fs = require('fs');
 var socketio = require("socket.io")(http);
 var child_process = require("child_process");
 
-function readCookie(cookies, cookieName)
+function printError(e)
 {
-    c = cookies.split(";");
-    for (var i = 0; i < c.length; i++)
-    {
-        s = c[i].trim().split("=")
-        if (s[0] == cookieName)
-            return s[1];
-    }
-    return null;
-}
+    console.log(e.message + " " + e.stack);
+};
+
+let messageQueue = [];
 
 socketio.on("connection", function (socket)
 {
@@ -27,31 +22,62 @@ socketio.on("connection", function (socket)
     {
         try
         {
-            console.log("got message from " + userName + ": " + messageContent);
-            // backendProcess.stdin.write("msg " + userName + ": " + messageContent);
-            socketio.emit("server2clientMessage", userName + ": " + messageContent); // send to all connected sockets
+            console.log("got message from " + userName + ": " + messageContent + ". Adding to queue.");
+
+            if (messageQueue.length > 10)
+                socket.emit("server2clientMessage", "can't send message '" + messageContent + "', man, there are too many in queue.");
+            else
+                messageQueue.push(userName + ": " + messageContent);
         }
         catch (e)
         {
-            console.log(e.message + " " + e.stack);
+            printError(e);
         }
     });
 });
 
+setInterval(() =>
+{
+    try
+    {
+        let messageToSend = messageQueue.shift();
+        if (messageToSend == undefined)
+            return;
+
+            // backendProcess.stdin.write("msg " + messageToSend);
+        socketio.emit("server2clientMessage", messageToSend); // send to all connected sockets
+    }
+    catch (e)
+    {
+        printError(e);
+    }
+
+}, 1000);
+
 app.get("/", function (req, res)
 {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile("static/home.htm", function (err, data)
+    try
     {
-        if (err) res.end(err);
-        else res.end(data);
-    });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile("static/home.htm", function (err, data)
+        {
+            if (err) res.end(err);
+            else res.end(data);
+        });
+    }
+    catch (e)
+    {
+        printError(e);
+    }
 });
 
-setInterval(() => {
-    console.log("trying to send message to clients");
-    socketio.emit("server2clientMessage", "suck my dicko"); // send to all connected sockets
-}, 1000);
+// let i = 0;
+// setInterval(() => {
+//     console.log("trying to send message to clients");
+//     socketio.emit("server2clientMessage", "suck my dicko " + i++); // send to all connected sockets
+// }, 1000);
+
+
 
 // var backendProcess = child_process.spawn("/src/src/cli/cli");
 
--- src/web/static/gui.js
+++ src/web/static/gui.js
@@ -4,14 +4,24 @@ $(function ()
 	let btnSend = document.getElementById("btnSend");
 	let txtUsername = document.getElementById("txtUsername");
 	let txtMessage = document.getElementById("txtMessage");
+	let divLog = document.getElementById("divLog");
+
 	btnSend.addEventListener("click", function ()
 	{
 		gikoClient.sendMessage(txtUsername.value, txtMessage.value);
 	});
 
+	let messageList = [];
+
 	gikoClient.setMessageHandler((message) =>
 	{
 		console.log("arrivato messaggio " + message);
+		messageList.push(message);
+		if (messageList.length > 10)
+			messageList = messageList.slice(1); // Removes the first value
+		divLog.innerHTML = messageList.join("<br />");
+		// Scroll to bottom. Might be nice to avoid that if the log wasn't already scrolled to bottom.
+		divLog.scrollTop = divLog.scrollHeight;
 	});
 }
 );
